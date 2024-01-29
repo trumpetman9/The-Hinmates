45a362b8bba5b1859bb189bbb17f1a30c79c8b74
mylee2662 <67172710+mylee2662@users.noreply.github.com>
2024-01-28 20:30:11 -0600

    wall slide

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
     public float speed;      
     
+    [Header("Jump")]
     public float jumpForce;             
     public float fallMultiplier;        //Gravity multiplier applied when player falls
     public float lowJumpMultiplier;     //Gravity multiplier applied when jump button is tapped (short jump)
     private Vector2 fallVector;         //Precalculated vector to account for increased gravity during player's fall
     private Vector2 lowJumpVector;      //Precalculated vector to allow player to make short jumps
 
+    [Header("Ground Check")]
     public float groundCheckRadius;     //Radius of ground check sphere
-    public LayerMask groundLayer;
     public Vector2 bottomOffset;       //Offset from player's transform to perform ground checks
+    public LayerMask groundLayer;
     private bool onGround;
+
+    [Header("Wall Climbing")]
+    public float wallCheckRadius;     //Radius of ground check sphere
+    public float slideVelocity;
+    public Vector2 sideOffset;       //Offset from player's transform to perform ground checks
+    private bool onWall;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -30,8 +39,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector2 inputDir = new Vector2(xInput, yInput);
+
+        Move(inputDir);
+
         //On ground check
         onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, groundCheckRadius, groundLayer);
+        //On wall check
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + sideOffset, wallCheckRadius, groundLayer) ||
+            Physics2D.OverlapCircle((Vector2)transform.position - sideOffset, wallCheckRadius, groundLayer);
+
+        if (onWall && !onGround)
+        {
+            Debug.Log("Wall Slide");
+            WallSlide();
+        }
 
         //Increase gravity by fallMultiplier if player is falling
         if(rb.velocity.y < 0)
@@ -43,12 +67,6 @@ public class Player : MonoBehaviour
         {
             rb.velocity += lowJumpVector * Time.deltaTime;
         }
-
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-        Vector2 inputDir = new Vector2(xInput, yInput);
-
-        Move(inputDir);
 
         if (onGround && Input.GetKeyDown(KeyCode.Space))
         {
@@ -67,9 +85,17 @@ public class Player : MonoBehaviour
         rb.velocity += Vector2.up * jumpForce;
     }
 
+    private void WallSlide()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, -slideVelocity);
+    }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
+        //Ground check sphere
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, groundCheckRadius);
+        //Wall check spheres
+        Gizmos.DrawWireSphere((Vector2)transform.position + sideOffset, wallCheckRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position - sideOffset, wallCheckRadius);
     }
 }
