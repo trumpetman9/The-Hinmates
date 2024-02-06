1f2ad48467beef4429e8cb1c888c2681fc235d21
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2024-02-05 18:04:49 -0600

    Basic basic combat

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeToAttack;
+    public float startTimeAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public float damage;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeToAttack <= 0)
+        {
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().health -= damage;
+                }
+
+            }
+            timeToAttack = startTimeAttack;
+
+        }
+        else
+        {
+            timeToAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        // Draw the attack range circle in the Scene view when the script is not running
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
